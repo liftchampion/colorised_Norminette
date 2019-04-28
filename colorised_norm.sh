163834abec936e6aa75b7e1b1d5b0ff7b4ea724a
ggerardy <liftchampion@gmail.com>
2019-04-28 20:55:52 +0300

    Fix link to installer. Fin?

--- colorised_norm.sh
+++ colorised_norm.sh
@@ -6,7 +6,7 @@
 #    By: ggerardy <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/04/25 18:53:10 by ggerardy          #+#    #+#              #
-#    Updated: 2019/04/28 20:36:20 by ggerardy         ###   ########.fr        #
+#    Updated: 2019/04/28 20:55:30 by ggerardy         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,8 +18,7 @@
 
 # ! better way to change colors:
 #	use installer:
-# curl https://bitbucket.org/liftchampion/colorised-norminette/raw/c81b549a582098ad560583aa7ec58a7a298429b4/colorised_norm_install_installer.sh | sh
-
+# sh -c "$(curl -s https://bitbucket.org/liftchampion/colorised-norminette/raw/b4272f2c2dac52b1da721ae658815c43f64e5cb2/colorised_norm_install_installer.sh)"
 NORM_COLOR=29
 NO_NORM_COLOR=202
 
