299a3ee5afc55626a7b7ffae19066cb7757beaa3
Alexander Chetverkin <virtual-ins@mail.ru>
2021-04-09 22:30:13 +0300

    Update for norminette 3.2.7

--- colorised_norm.sh
+++ colorised_norm.sh
@@ -6,7 +6,7 @@
 #    By: ggerardy <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/04/25 18:53:10 by ggerardy          #+#    #+#              #
-#    Updated: 2019/04/28 20:55:30 by ggerardy         ###   ########.fr        #
+#    Updated: 2021/05/06 05:17:28 by amalliar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,39 +19,23 @@
 # ! better way to change colors:
 #	use installer:
 # sh -c "$(curl -s https://bitbucket.org/liftchampion/colorised-norminette/raw/b4272f2c2dac52b1da721ae658815c43f64e5cb2/colorised_norm_install_installer.sh)"
-NORM_COLOR=29
-NO_NORM_COLOR=202
+NORM_COLOR=10
+NO_NORM_COLOR=9
 
-norminette $* | awk -v nc="$NORM_COLOR" -v nnc="$NO_NORM_COLOR" '
-BEGIN {buf = "lol"}
-{
-    if (buf != "lol" && $1 == "Norme:")
-    {
-        if (buf ~ /Norme:*/)
-			print "\x1B[38;5;" nc "m" buf "\x1B[0m"
-        else
-            print buf
-    }
-    else if (buf != "lol")
-    {
-        if (buf ~ /Norme:*/)
-            print "\x1B[38;5;" nnc "m" buf "\x1B[0m"
-        else
-            print buf
-    }
-    buf = $0
+python -m norminette "$@" | awk -v nc="$NORM_COLOR" -v nnc="$NO_NORM_COLOR" '
+BEGIN {
+    nc = "\x1b[38;5;" nc "m"
+    nnc = "\x1b[38;5;" nnc "m"
+    noc = "\x1b[0m"
 }
-END {
-    if (buf != "lol")
-    {
-        if (buf ~ /Norme:*/)
-        {
-            print "\x1B[38;5;" nc "m" buf "\x1B[0m"
-        }
-        else
-        {
-            print buf
-        }
-    }
+{
+    output = $0
+
+    if (output ~ /OK!/)
+        print nc output noc
+    else if (output ~ /Error!/)
+        print nnc output noc
+    else
+        print output
 }
 '
